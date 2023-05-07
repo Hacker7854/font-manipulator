2962d88615dacf489668b1eaa58dade2e339d5ab
Hacker7854 <96180102+Hacker7854@users.noreply.github.com>
2023-05-07 15:27:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+noseX=0;
+noseY=0;
+function preload(){
+
+}
+
+
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = "+ noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWrist + "difference =" + difference);
+    }
+}
+
